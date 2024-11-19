724630177200e979e6af2aad57e74e5172d2df60
mohsen-pashootan <tahmasebpour.mohsen@gmail.com>
2024-11-19 10:30:39 +0330

    add proof of work function

--- dev/blockchain.js
+++ dev/blockchain.js
@@ -1,6 +1,11 @@
+const sha256 = require("sha256");
+
 function Blockchain() {
   this.chain = [];
   this.pendingTransactions = [];
+
+  // Genesis Block
+  this.createNewBlock(100, "0", "0");
 }
 
 Blockchain.prototype.createNewBlock = function (
@@ -41,6 +46,32 @@ Blockchain.prototype.createNewTransaction = function (
   this.pendingTransactions.push(newTransaction);
 
   return this.getLastBlock()["index"] + 1;
+};
+
+Blockchain.prototype.hashBlock = function (
+  previousBlockHash,
+  currentBlockData,
+  nonce
+) {
+  const dataAsString =
+    previousBlockHash + nonce.toString() + JSON.stringify(currentBlockData);
+  const hash = sha256(dataAsString);
+  return hash;
+};
+
+// actualy proofOfWork is the value of nonce
+Blockchain.prototype.proofOfWork = function (
+  previousBlockHash,
+  currentBlockData
+) {
+  let nonce = 0;
+  let hash = this.hashBlock(previousBlockHash, currentBlockData, nonce);
+  while (hash.substring(0, 4) !== "0000") {
+    nonce++;
+    hash = this.hashBlock(previousBlockHash, currentBlockData, nonce);
+  }
+
+  return nonce;
 };
 
 module.exports = Blockchain;
