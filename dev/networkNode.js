7860b14c6eacbbc87daaaa483cda9f5876753e4f
mohsen-pashootan <tahmasebpour.mohsen@gmail.com>
2024-11-20 13:31:23 +0330

    add block explore ui endpoint

--- dev/networkNode.js
+++ dev/networkNode.js
@@ -259,6 +259,11 @@ app.get("/address/:address", function (req, res) {
   });
 });
 
+// block explorer
+app.get("/block-explorer", function (req, res) {
+  res.sendFile("./block-explorer/index.html", { root: __dirname });
+});
+
 app.listen(port, function () {
   console.log(`Listening on port ${port}...`);
 });
